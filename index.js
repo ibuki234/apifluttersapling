c702c1d9aff1bc0092a604bb80fd99b62dc33d1f
cira1234 <aunoford89@gmail.com>
2023-07-14 09:00:23 +0700

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express') 
-const cors =require('cors')
-const mysql =require('mysql2')
-require('dotenv').config()
+const cors =require('cors');
+const mysql =require('mysql2');
+require('dotenv').config();
 const app=express()
 
 
 app.use(cors())
 
-const connection = mysql.createConnection(process.env.DATABASE_URL)
+const connection = mysql.createConnection(
+process.env.DATABASE_URL
+)
 
 app.get('/',(req,res) => {
     console.log('Hi')
@@ -22,6 +24,23 @@ app.get('/selectuser',(req,res) =>{
             res.send(results)
         }
     )
+}),
+
+    app.get('/insertuser/:email/:pass/:name/:tel/:imguser',(req,res) =>{
+    var email=req.params.email;
+     var pass=req.params.pass;
+     var name=req.params.name;
+     var tel=req.params.tel;
+      var img=req.params.imguser;
+    connection.query(
+        'insert into user(email,pass,name,tel,imguser) values(?,?,?,?,?)',
+        [email,pass,name,tel,img],
+        function(err,results,fields){
+            console.log(results )
+            res.send(results)
+              console.log('insert success');
+        }
+    )
 })
 
-app.listen(process.env.POR || 3000) 
+// app.listen(process.env.POR || 3000) 
