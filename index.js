1b567509e2195fb5828d9f4824080db72772b504
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-08-05 09:50:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,30 @@ const app=express()
 
 app.use(cors())
 
+function performSignIn() {
 
+  let headers = new Headers();
+
+  headers.append('Content-Type', 'application/json');
+  headers.append('Accept', 'application/json');
+
+  headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+  headers.append('Access-Control-Allow-Credentials', 'true');
+
+  headers.append('GET', 'POST', 'OPTIONS');
+
+  headers.append('Authorization', 'Basic ' + base64.encode(username + ":" + password));
+
+  fetch(sign_in, {
+      //mode: 'no-cors',
+      credentials: 'include',
+      method: 'POST',
+      headers: headers
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log('Authorization failed : ' + error.message));
+}
 const storage=multer.diskStorage({
     destination:function(req,file,callback){
         callback(null,__dirname+"/uploads");
