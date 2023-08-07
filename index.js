3f608626f0168e90f9ca5bc3093b6d04dc5a4a01
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-08-07 21:22:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ app.get('/deleteuser/:id',(req,res) =>{
     )
 }),
 
-    app.get('/id/:email/:pass',(req,res) =>{
+    app.get('/a/:email/:pass',(req,res) =>{
         var email=req.params.email;
         var pass=req.params.pass;
     connection.query(
