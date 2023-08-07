80c6b05a1c64b20c2027543252c0b8703c84d7a4
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-08-07 08:37:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ require('dotenv').config();
 const app=express()
 
 
-app.use(cors())
+// app.use(cors())
 
 
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const storage=multer.diskStorage({
     destination:function(req,file,callback){
         callback(null,__dirname+"/uploads");
