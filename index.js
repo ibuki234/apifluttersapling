f2f768f2c512cc537ed9d4505d0a9f6d4b477741
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-08-11 12:13:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ app.get('/selectmember',(req,res) =>{
         [section,question,img],
         function(err,results,fields){
             console.log(results )
-            res.send('insertcomplete')
+            res.send(results)
               console.log('insert success');
         }
     )
